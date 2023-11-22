befeaf23394c017964e629991b3f7ddcb2015f7b
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-22 09:00:25 +0100

    Clean up index.js

--- index.js
+++ index.js
@@ -82,33 +82,8 @@ function createWeatherCard(dayData, isFirstCard) {
   container.appendChild(card);
 }
 
-// const appWeather = async () => {
-//   const theNameOfTheCity = inputField.value.trim();
-
-//   try {
-//     const getData = await getWeatherData(theNameOfTheCity);
-//     console.log("Get all the data", getData);
-
-//     if (theNameOfTheCity) {
-//       container.innerHTML = "";
-//       cityNameContainer.innerHTML = "";
-
-//       const locationParagraph = document.createElement("p");
-//       locationParagraph.textContent = `${getData.location.name}, ${getData.location.country}`;
-//       cityNameContainer.appendChild(locationParagraph);
-
-//       for (let i = 0; i < 7; i++) {
-//         createWeatherCard(getData.forecast.forecastday[i], i === 0);
-//       }
-//     }
-//   } catch (error) {
-//     alert("Something is wrong read your console errors bro");
-//     console.error("Error fetching weather data :", error);
-//   }
-// };
-
 inputField.addEventListener("keyup", function (event) {
-  if (event.keyup === "Enter") {
+  if (event.key === "Enter") {
     appWeather();
   }
 });
